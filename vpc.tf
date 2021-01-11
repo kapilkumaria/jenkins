0a33efc2d07055604793ca4fa545ddc9b6cc7ced
kapil <kapil.kumaria@gmail.com>
2021-01-11 20:14:41 +0000

    added kkprofile

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,10 @@
 provider "aws" {
     region = "ca-central-1"
+    shared_credentials_file = "/root/.aws/credentials"
+    profile = "kkprofile"
+
 }
+
 
 resource "aws_vpc" "myvpc" {
     cidr_block = "10.0.0.0/26"
